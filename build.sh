44096e093e737ef4a4e3f53d09d0b689d519903d
JJTech0130 <jjtech@jjtech.dev>
2023-05-17 19:57:19 -0400

    implement reading of data.plist in VIOKit

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ xcrun clang -shared stubs.o -o STUB -framework Foundation -arch x86_64
 rm stubs.o
 
 # Compile the viokit.m file into a .o file
-xcrun clang -c viokit.m -o viokit.o -arch x86_64
+xcrun clang -c viokit.m -o viokit.o -arch x86_64 -fobjc-arc
 # Link the viokit.o file into the output file
 xcrun clang -shared viokit.o -o VIOKit -framework Foundation -arch x86_64
 # Remove the viokit.o file
 rm viokit.o
 
 # Compile test.m into an executable
-xcrun clang demo.m -o demo -framework Foundation -framework IOKit -arch x86_64
+xcrun clang demo.m -o demo -framework Foundation -framework IOKit -arch x86_64 -fobjc-arc
 # Run the executable
 ./demo
--- viokit.m
+++ viokit.m
@@ -1,57 +1,35 @@
 #include <CoreFoundation/CoreFoundation.h>
+//#include <Foundation/Foundation.h>
 #include <IOKit/IOTypes.h>
 #include <stdio.h>
-
-// Fake data
-#define FAKE_MODEL "MacPro5,1"
-#define FAKE_BOARD_ID "Mac-F221BEC8"
-#define FAKE_SERIAL "CK1350NCEUH"
-#define FAKE_HW_UUID "ABB178CD-25C5-5AFB-A749-B432FD683AE1"
-// #define FAKE_SYS_UUID "ADEAE"
-const uint64_t FAKE_ROM = 0xb48b1988b880;
-#define FAKE_BOARD_SERIAL "CK1350351BH8U"
-
-// #define FAKE_G_DATA [0x1663a52f1d827299e41d56d74223ecca60]
-// const uint8_t[] FAKE_G_DATA = uint8_t[0x16, 0x63, 0xa5, 0x2f, 0x1d, 0x82,
-// 0x72, 0x99, 0xe4, 0x1d, 0x56, 0xd7, 0x42, 0x23, 0xec, 0xca, 0x60]
-//const uint8_t FAKE_G_DATA[] = {0x16, 0x63, 0xa5, 0x2f, 0x1d, 0x82,
-//                               0x72, 0x99, 0xe4, 0x1d, 0x56, 0xd7,
-//                               0x42, 0x23, 0xec, 0xca, 0x60};
-#define BA(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p) {0x##a,0x##b,0x##c,0x##d,\
-0x##e,0x##f,0x##g,0x##h,0x##i,0x##j,0x##k,0x##l,0x##m,0x##n,0x##o,0x##p}
-
-#define TO_BYTEARRAY(...) BA(__VA_ARGS__)
-
-const uint8_t FAKE_G_DATA[] = TO_BYTEARRAY();
-
-
-// const uint8_t[] fake_g_data = FAKE_G_DATA;
-// #define FAKE_F_DATA [0x95987adcec1a31eccd63a9ba21e182fadb]
-// #define FAKE_F_DATA [0x95, 0x98, 0x7a, 0xdc, 0xec, 0x1a, 0x31, 0xec, 0xcd,
-// 0x63, 0xa9, 0xba, 0x21, 0xe1, 0x82, 0xfa, 0xdb]
-const uint8_t FAKE_F_DATA[] = {0x95, 0x98, 0x7a, 0xdc, 0xec, 0x1a,
-                               0x31, 0xec, 0xcd, 0x63, 0xa9, 0xba,
-                               0x21, 0xe1, 0x82, 0xfa, 0xdb};
-// #define FAKE_k_DATA [0x6e7092c5131ea9378f57e9d109ed010112]
-// #define FAKE_k_DATA [0x6e, 0x70, 0x92, 0xc5, 0x13, 0x1e, 0xa9, 0x37, 0x8f,
-// 0x57, 0xe9, 0xd1, 0x09, 0xed, 0x01, 0x01, 0x12]
-const uint8_t FAKE_k_DATA[] = {0x6e, 0x70, 0x92, 0xc5, 0x13, 0x1e,
-                               0xa9, 0x37, 0x8f, 0x57, 0xe9, 0xd1,
-                               0x09, 0xed, 0x01, 0x01, 0x12};
-// #define FAKE_o_DATA [0x71dbaec99c49cef0a5addcb0e2be0b2f2d]
-// #define FAKE_o_DATA [0x71, 0xdb, 0xae, 0xc9, 0x9c, 0x49, 0xce, 0xf0, 0xa5,
-// 0xad, 0xdc, 0xb0, 0xe2, 0xbe, 0x0b, 0x2f, 0x2d]
-const uint8_t FAKE_o_DATA[] = {0x71, 0xdb, 0xae, 0xc9, 0x9c, 0x49,
-                               0xce, 0xf0, 0xa5, 0xad, 0xdc, 0xb0,
-                               0xe2, 0xbe, 0x0b, 0x2f, 0x2d};
-// #define FAKE_a_DATA [0x9d94ed553d6d55a09074f667aa6b6cd155]
-// #define FAKE_a_DATA [0x9d, 0x94, 0xed, 0x55, 0x3d, 0x6d, 0x55, 0xa0, 0x90,
-// 0x74, 0xf6, 0x67, 0xaa, 0x6b, 0x6c, 0xd1, 0x55]
-const uint8_t FAKE_a_DATA[] = {0x9d, 0x94, 0xed, 0x55, 0x3d, 0x6d,
-                               0x55, 0xa0, 0x90, 0x74, 0xf6, 0x67,
-                               0xaa, 0x6b, 0x6c, 0xd1, 0x55};
+// #include <Foundation/Foundation.h>
+#include <Foundation/NSData.h>
+#include <Foundation/NSPropertyList.h>
+#include <Foundation/NSString.h>
+#include <Foundation/NSDictionary.h>
 
 // Helpers
+NSDictionary* read_data_plist() {
+  // Read data.plist from the current directory
+  NSString* path = @"data.plist";
+  NSData* data = [NSData dataWithContentsOfFile:path];
+  if (!data) {
+    printf("Failed to read data.plist\n");
+    return nil;
+  }
+  NSError* error;
+  NSDictionary* plist = [NSPropertyListSerialization
+      propertyListWithData:data
+                   options:NSPropertyListImmutable
+                    format:NULL
+                     error:&error];
+  if (error) {
+    printf("Failed to parse data.plist\n");
+    return nil;
+  }
+  return plist;
+}
+
 #define CFSTR_CMP(str1, str2) CFStringCompare(str1, str2, 0) == 0
 
 CFDataRef data_from_cfstr(CFStringRef string) {
@@ -82,6 +60,8 @@ io_registry_entry_t IORegistryEntryFromPath(mach_port_t masterPort,
   return 91;
 }
 
+NSDictionary* DATA_PLIST = nil;
+
 CFTypeRef IORegistryEntryCreateCFProperty(io_registry_entry_t entry,
                                           CFStringRef key,
                                           CFAllocatorRef allocator,
@@ -91,40 +71,90 @@ CFTypeRef IORegistryEntryCreateCFProperty(io_registry_entry_t entry,
   CFStringGetCString(key, key_c, 100, kCFStringEncodingUTF8);
   printf("IORegistryEntryCreateCFProperty called with entry: %d key: %s\n",
          entry, key_c);
-  // Check the key
-  if (CFSTR_CMP(key, CFSTR("board-id"))) {
-    return data_from_cfstr(CFSTR(FAKE_BOARD_ID));
-  } else if (CFSTR_CMP(key, CFSTR("product-name"))) {
-    return data_from_cfstr(CFSTR(FAKE_MODEL));
-  } else if (CFSTR_CMP(key, CFSTR("IOPlatformSerialNumber"))) {
-    return CFSTR(FAKE_SERIAL);
-  } else if (CFSTR_CMP(key, CFSTR("IOPlatformUUID"))) {
-    return CFSTR(FAKE_HW_UUID);
-  } else if (CFSTR_CMP(key, CFSTR(G_DATA_NAME))) {
-    // Create CFDataRef from FAKE_G_DATA
-    return CFDataCreate(NULL, (const UInt8 *)&FAKE_G_DATA, sizeof(FAKE_G_DATA));
-  } else if (CFSTR_CMP(key, CFSTR(F_DATA_NAME))) {
-    // Create CFDataRef from FAKE_F_DATA
-    return CFDataCreate(NULL, (const UInt8 *)&FAKE_F_DATA, sizeof(FAKE_F_DATA));
-  } else if (CFSTR_CMP(key, CFSTR(k_DATA_NAME))) {
-    // Create CFDataRef from FAKE_k_DATA
-    return CFDataCreate(NULL, (const UInt8 *)&FAKE_k_DATA, sizeof(FAKE_k_DATA));
-  } else if (CFSTR_CMP(key, CFSTR("IOMACAddress"))) {
-    // Create CFDataRef from FAKE_ROM, which is a uint64_t, but we only want the
-    // 6 least significant bytes
-    return CFDataCreate(NULL, (const UInt8 *)&FAKE_ROM + 2, 6);
+
+  // Check if the key is in data.plist
+  if (DATA_PLIST == nil) {
+    DATA_PLIST = read_data_plist();
+    if (!DATA_PLIST) {
+      printf("Failed to read data.plist\n");
+      return NULL;
+    }
   }
-  /*else if (CFSTR_CMP(key, CFSTR(o_DATA_NAME))) {
-   // Create CFDataRef from FAKE_o_DATA
-   return CFDataCreate(NULL, (const UInt8 *)&FAKE_o_DATA, sizeof(FAKE_o_DATA));
- } else if (CFSTR_CMP(key, CFSTR(a_DATA_NAME))) {
-   // Create CFDataRef from FAKE_a_DATA
-   return CFDataCreate(NULL, (const UInt8 *)&FAKE_a_DATA, sizeof(FAKE_a_DATA));
- }*/
-  else {
+  NSDictionary* data_plist = DATA_PLIST;
+  // Convert the CFStringRef to a NSString
+  NSString* key_ns = (__bridge_transfer NSString*)key;
+  // Check if the key is in the dictionary
+  if ([data_plist objectForKey:key_ns]) {
+    // Get the value
+    id value = [data_plist objectForKey:key_ns];
+    // Check if it is a string
+    if ([value isKindOfClass:[NSString class]]) {
+      // Convert the NSString to a CFStringRef
+      CFStringRef value_cf = (__bridge_retained CFStringRef)value;
+      // Create a CFDataRef from the CFStringRef
+      CFDataRef value_data = data_from_cfstr(value_cf);
+      // Release the CFStringRef
+      CFRelease(value_cf);
+      // Return the CFDataRef
+      return value_data;
+    } else if ([value isKindOfClass:[NSNumber class]]) {
+      // Convert the NSNumber to a CFNumberRef
+      CFNumberRef value_cf = (__bridge_retained CFNumberRef)value;
+      // Create a CFDataRef from the CFNumberRef
+      CFDataRef value_data = CFDataCreate(NULL, (const UInt8 *)&value_cf,
+                                          sizeof(value_cf));
+      // Release the CFNumberRef
+      CFRelease(value_cf);
+      // Return the CFDataRef
+      return value_data;
+    } else if ([value isKindOfClass:[NSData class]]) {
+      // Convert the NSData to a CFDataRef
+      CFDataRef value_cf = (__bridge_retained CFDataRef)value;
+      // Return the CFDataRef
+      return value_cf;
+    } else {
+      // Return NULL
+      return NULL;
+    }
+  } else {
     // Return NULL
     return NULL;
   }
+
+  // Check the key
+//   if (CFSTR_CMP(key, CFSTR("board-id"))) {
+//     return data_from_cfstr(CFSTR(FAKE_BOARD_ID));
+//   } else if (CFSTR_CMP(key, CFSTR("product-name"))) {
+//     return data_from_cfstr(CFSTR(FAKE_MODEL));
+//   } else if (CFSTR_CMP(key, CFSTR("IOPlatformSerialNumber"))) {
+//     return CFSTR(FAKE_SERIAL);
+//   } else if (CFSTR_CMP(key, CFSTR("IOPlatformUUID"))) {
+//     return CFSTR(FAKE_HW_UUID);
+//   } else if (CFSTR_CMP(key, CFSTR(G_DATA_NAME))) {
+//     // Create CFDataRef from FAKE_G_DATA
+//     return CFDataCreate(NULL, (const UInt8 *)&FAKE_G_DATA, sizeof(FAKE_G_DATA));
+//   } else if (CFSTR_CMP(key, CFSTR(F_DATA_NAME))) {
+//     // Create CFDataRef from FAKE_F_DATA
+//     return CFDataCreate(NULL, (const UInt8 *)&FAKE_F_DATA, sizeof(FAKE_F_DATA));
+//   } else if (CFSTR_CMP(key, CFSTR(k_DATA_NAME))) {
+//     // Create CFDataRef from FAKE_k_DATA
+//     return CFDataCreate(NULL, (const UInt8 *)&FAKE_k_DATA, sizeof(FAKE_k_DATA));
+//   } else if (CFSTR_CMP(key, CFSTR("IOMACAddress"))) {
+//     // Create CFDataRef from FAKE_ROM, which is a uint64_t, but we only want the
+//     // 6 least significant bytes
+//     return CFDataCreate(NULL, (const UInt8 *)&FAKE_ROM + 2, 6);
+//   }
+//   /*else if (CFSTR_CMP(key, CFSTR(o_DATA_NAME))) {
+//    // Create CFDataRef from FAKE_o_DATA
+//    return CFDataCreate(NULL, (const UInt8 *)&FAKE_o_DATA, sizeof(FAKE_o_DATA));
+//  } else if (CFSTR_CMP(key, CFSTR(a_DATA_NAME))) {
+//    // Create CFDataRef from FAKE_a_DATA
+//    return CFDataCreate(NULL, (const UInt8 *)&FAKE_a_DATA, sizeof(FAKE_a_DATA));
+//  }*/
+//   else {
+//     // Return NULL
+//     return NULL;
+//   }
 }
 
 CFMutableDictionaryRef IOServiceMatching(const char *name) {
