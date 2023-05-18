c74b4619de9696e17b921238b9b523808977728c
JJTech0130 <jjtech@jjtech.dev>
2023-05-18 13:39:10 -0400

    formatting etc

--- viokit.m
+++ viokit.m
@@ -1,29 +1,26 @@
 #include <CoreFoundation/CoreFoundation.h>
-//#include <Foundation/Foundation.h>
-//#include <Foundation/Foundation.h>
-#include <IOKit/IOTypes.h>
-#include <stdio.h>
-// #include <Foundation/Foundation.h>
 #include <Foundation/NSData.h>
+#include <Foundation/NSDictionary.h>
 #include <Foundation/NSPropertyList.h>
 #include <Foundation/NSString.h>
-#include <Foundation/NSDictionary.h>
+#include <IOKit/IOTypes.h>
+#include <stdio.h>
 
 // Helpers
-NSDictionary* read_data_plist() {
+NSDictionary *read_data_plist() {
   // Read data.plist from the current directory
-  NSString* path = @"data.plist";
-  NSData* data = [NSData dataWithContentsOfFile:path];
+  NSString *path = @"data.plist";
+  NSData *data = [NSData dataWithContentsOfFile:path];
   if (!data) {
     printf("Failed to read data.plist\n");
     return nil;
   }
-  NSError* error;
-  NSDictionary* plist = [NSPropertyListSerialization
-      propertyListWithData:data
-                   options:NSPropertyListImmutable
-                    format:NULL
-                     error:&error];
+  NSError *error;
+  NSDictionary *plist =
+      [NSPropertyListSerialization propertyListWithData:data
+                                                options:NSPropertyListImmutable
+                                                 format:NULL
+                                                  error:&error];
   if (error) {
     printf("Failed to parse data.plist\n");
     return nil;
@@ -46,12 +43,6 @@ CFDataRef data_from_cfstr(CFStringRef string) {
 }
 
 // Stubs
-#define G_DATA_NAME "Gq3489ugfi"
-#define F_DATA_NAME "Fyp98tpgj"
-#define k_DATA_NAME "kbjfrfpoJU"
-#define o_DATA_NAME "oycqAZloTNDm"
-#define a_DATA_NAME "abKPld1EcMni"
-
 mach_port_t kIOMasterPortDefault = 90;
 
 io_registry_entry_t IORegistryEntryFromPath(mach_port_t masterPort,
@@ -61,7 +52,7 @@ io_registry_entry_t IORegistryEntryFromPath(mach_port_t masterPort,
   return 91;
 }
 
-NSDictionary* DATA_PLIST = nil;
+NSDictionary *DATA_PLIST = nil;
 
 CFTypeRef IORegistryEntryCreateCFProperty(io_registry_entry_t entry,
                                           CFStringRef key,
@@ -81,37 +72,21 @@ CFTypeRef IORegistryEntryCreateCFProperty(io_registry_entry_t entry,
       return NULL;
     }
   }
-  NSDictionary* data_plist = DATA_PLIST;
+  NSDictionary *data_plist = DATA_PLIST;
   // Convert the CFStringRef to a NSString
-  NSString* key_ns = (__bridge_transfer NSString*)key;
+  NSString *key_ns = (__bridge_transfer NSString *)key;
   // Check if the key is in the dictionary
   if ([data_plist objectForKey:key_ns]) {
     // Get the value
     id value = [data_plist objectForKey:key_ns];
-    //printf("value: %p\n", value);
+    // printf("value: %p\n", value);
     NSLog(@"value: %@", value);
     // Check if it is a string
     if ([value isKindOfClass:[NSString class]]) {
       printf("value is NSString\n");
       // Convert the NSString to a CFStringRef
       CFStringRef value_cf = (__bridge_retained CFStringRef)value;
-      // Create a CFDataRef from the CFStringRef
-      //CFDataRef value_data = data_from_cfstr(value_cf);
-      // Release the CFStringRef
-      //CFRelease(value_cf);
-      // Return the CFDataRef
       return value_cf;
-    } else if ([value isKindOfClass:[NSNumber class]]) {
-      printf("value is NSNumber\n");
-      // Convert the NSNumber to a CFNumberRef
-      CFNumberRef value_cf = (__bridge_retained CFNumberRef)value;
-      // Create a CFDataRef from the CFNumberRef
-      CFDataRef value_data = CFDataCreate(NULL, (const UInt8 *)&value_cf,
-                                          sizeof(value_cf));
-      // Release the CFNumberRef
-      CFRelease(value_cf);
-      // Return the CFDataRef
-      return value_data;
     } else if ([value isKindOfClass:[NSData class]]) {
       printf("value is NSData\n");
       // Convert the NSData to a CFDataRef
@@ -128,41 +103,6 @@ CFTypeRef IORegistryEntryCreateCFProperty(io_registry_entry_t entry,
     // Return NULL
     return NULL;
   }
-
-  // Check the key
-//   if (CFSTR_CMP(key, CFSTR("board-id"))) {
-//     return data_from_cfstr(CFSTR(FAKE_BOARD_ID));
-//   } else if (CFSTR_CMP(key, CFSTR("product-name"))) {
-//     return data_from_cfstr(CFSTR(FAKE_MODEL));
-//   } else if (CFSTR_CMP(key, CFSTR("IOPlatformSerialNumber"))) {
-//     return CFSTR(FAKE_SERIAL);
-//   } else if (CFSTR_CMP(key, CFSTR("IOPlatformUUID"))) {
-//     return CFSTR(FAKE_HW_UUID);
-//   } else if (CFSTR_CMP(key, CFSTR(G_DATA_NAME))) {
-//     // Create CFDataRef from FAKE_G_DATA
-//     return CFDataCreate(NULL, (const UInt8 *)&FAKE_G_DATA, sizeof(FAKE_G_DATA));
-//   } else if (CFSTR_CMP(key, CFSTR(F_DATA_NAME))) {
-//     // Create CFDataRef from FAKE_F_DATA
-//     return CFDataCreate(NULL, (const UInt8 *)&FAKE_F_DATA, sizeof(FAKE_F_DATA));
-//   } else if (CFSTR_CMP(key, CFSTR(k_DATA_NAME))) {
-//     // Create CFDataRef from FAKE_k_DATA
-//     return CFDataCreate(NULL, (const UInt8 *)&FAKE_k_DATA, sizeof(FAKE_k_DATA));
-//   } else if (CFSTR_CMP(key, CFSTR("IOMACAddress"))) {
-//     // Create CFDataRef from FAKE_ROM, which is a uint64_t, but we only want the
-//     // 6 least significant bytes
-//     return CFDataCreate(NULL, (const UInt8 *)&FAKE_ROM + 2, 6);
-//   }
-//   /*else if (CFSTR_CMP(key, CFSTR(o_DATA_NAME))) {
-//    // Create CFDataRef from FAKE_o_DATA
-//    return CFDataCreate(NULL, (const UInt8 *)&FAKE_o_DATA, sizeof(FAKE_o_DATA));
-//  } else if (CFSTR_CMP(key, CFSTR(a_DATA_NAME))) {
-//    // Create CFDataRef from FAKE_a_DATA
-//    return CFDataCreate(NULL, (const UInt8 *)&FAKE_a_DATA, sizeof(FAKE_a_DATA));
-//  }*/
-//   else {
-//     // Return NULL
-//     return NULL;
-//   }
 }
 
 CFMutableDictionaryRef IOServiceMatching(const char *name) {
@@ -183,12 +123,6 @@ CFMutableDictionaryRef IOServiceMatching(const char *name) {
 
 io_service_t IOServiceGetMatchingService(mach_port_t masterPort,
                                          CFDictionaryRef matching) {
-  // Print the CFDictionary
-  // CFShow(matching);
-  // printf("IOServiceGetMatchingService called with port: %d matching: \n",
-  //      masterPort);
-  // CFShow(matching);
-
   // Get the IOProviderClass
   CFStringRef provider_class =
       CFDictionaryGetValue(matching, CFSTR("IOProviderClass"));
@@ -243,33 +177,39 @@ kern_return_t IORegistryEntryGetParentEntry(io_registry_entry_t entry,
 }
 
 // DISK ARBITRATION
-//#import <DiskArbitration/DiskArbitration.h>
+// #import <DiskArbitration/DiskArbitration.h>
 
-const CFStringRef kDADiskDescriptionVolumeUUIDKey = CFSTR("DADiskDescriptionVolumeUUIDKey");
+const CFStringRef kDADiskDescriptionVolumeUUIDKey =
+    CFSTR("DADiskDescriptionVolumeUUIDKey");
 
-CFNumberRef DASessionCreate(void * alloc) {
+CFNumberRef DASessionCreate(void *alloc) {
   printf("DASessionCreate called with alloc: %p\n", alloc);
   // Create a CFNumberRef
   // Create a CFNumberRef from 201
   int value = 201;
   CFNumberRef value_cf = CFNumberCreate(NULL, kCFNumberIntType, &value);
   return value_cf;
-  //return 201;
+  // return 201;
 }
 
-CFNumberRef DADiskCreateFromBSDName(CFAllocatorRef allocator, int session, const char * name) {
-  printf("DADiskCreateFromBSDName called with allocator: %p session: %d name: %s\n", allocator, session, name);
-  //return 202;
+CFNumberRef DADiskCreateFromBSDName(CFAllocatorRef allocator, int session,
+                                    const char *name) {
+  printf("DADiskCreateFromBSDName called with allocator: %p session: %d name: "
+         "%s\n",
+         allocator, session, name);
+  // return 202;
   int value = 202;
   CFNumberRef value_cf = CFNumberCreate(NULL, kCFNumberIntType, &value);
   return value_cf;
 }
 
-//DADiskCopyDescription(DADiskRef  _Nonnull disk)
-CFDictionaryRef DADiskCopyDescription( int disk ) {
+// DADiskCopyDescription(DADiskRef  _Nonnull disk)
+CFDictionaryRef DADiskCopyDescription(int disk) {
   printf("DADiskCopyDescription called with disk: %d\n", disk);
   // Create a CFMutableDictionaryRef
-  CFMutableDictionaryRef dict = CFDictionaryCreateMutable(NULL, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+  CFMutableDictionaryRef dict =
+      CFDictionaryCreateMutable(NULL, 0, &kCFTypeDictionaryKeyCallBacks,
+                                &kCFTypeDictionaryValueCallBacks);
   printf("dict: %p\n", dict);
   // Add the UUID to the dictionary
   // Load the UUID from data.plist
@@ -280,32 +220,24 @@ CFDictionaryRef DADiskCopyDescription( int disk ) {
       return NULL;
     }
   }
-  NSDictionary* data_plist = DATA_PLIST;
+  NSDictionary *data_plist = DATA_PLIST;
   printf("data_plist: %p\n", data_plist);
   // Get root_disk_uuid
-  NSString* root_disk_uuid = [data_plist objectForKey:@"root_disk_uuid"];
+  NSString *root_disk_uuid = [data_plist objectForKey:@"root_disk_uuid"];
   // Convert the NSString to a CFStringRef
   CFStringRef root_disk_uuid_cf = (__bridge_retained CFStringRef)root_disk_uuid;
   // Convert it to a CFUUIDRef
-  CFUUIDRef root_disk_uuid_cfuuid = CFUUIDCreateFromString(NULL, root_disk_uuid_cf);
+  CFUUIDRef root_disk_uuid_cfuuid =
+      CFUUIDCreateFromString(NULL, root_disk_uuid_cf);
   printf("root_disk_uuid_cf: %p\n", root_disk_uuid_cf);
   // Add the UUID to the dictionary
-  CFDictionaryAddValue(dict, kDADiskDescriptionVolumeUUIDKey, root_disk_uuid_cfuuid);
+  CFDictionaryAddValue(dict, kDADiskDescriptionVolumeUUIDKey,
+                       root_disk_uuid_cfuuid);
   printf("dict: %p\n", dict);
-  // Make a copy of the CFDictionaryRef so that we are not returning an ARC object
+  // Make a copy of the CFDictionaryRef so that we are not returning an ARC
+  // object
   CFDictionaryRef dict_copy = CFDictionaryCreateCopy(NULL, dict);
   printf("dict_copy: %p\n", dict_copy);
   // Return the dictionary
   return dict_copy;
 }
-//kDADiskDescriptionVolumeUUIDKey 
-// IOIteratorNext
-// IOObjectRelease
-// IORegistryEntryCreateCFProperty
-// IORegistryEntryFromPath
-// IORegistryEntryGetParentEntry
-// IOServiceGetMatchingService
-// IOServiceGetMatchingServices
-// IOServiceMatching
-
-// ALSO STUB DA IN SAME FILE?!
